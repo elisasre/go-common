ba2ed9b1aebbc3ba018adc14ec9f923794557e27
Juho Majasaari <ext-juho.majasaari@elisa.fi>
2022-05-19 13:12:25 +0300

    Add generic functions for values and pointers
    * Refactor old functions to use the generic ones for backwards
      compatibility.

--- types.go
+++ types.go
@@ -7,27 +7,39 @@ import (
 
 var supportedtruevalues = []string{"true", "t", "yes", "y", "on"}
 
+// Ptr returns pointer to any type.
+func Ptr[T any](v T) *T {
+	return &v
+}
+
+// PtrValue returns value of any type.
+func PtrValue[T any](p *T) T {
+	if p != nil {
+		return *p
+	}
+
+	var v T
+	return v
+}
+
 // String returns pointer to string.
 func String(s string) *string {
-	return &s
+	return Ptr(s)
 }
 
 // StringValue returns string value from pointervalue.
 func StringValue(s *string) string {
-	if s == nil {
-		return ""
-	}
-	return *s
+	return PtrValue(s)
 }
 
 // Int returns pointer to int.
 func Int(value int) *int {
-	return &value
+	return Ptr(value)
 }
 
 // Int64 returns pointer to int64.
 func Int64(value int64) *int64 {
-	return &value
+	return Ptr(value)
 }
 
 // MapToString modifies map to string array.
@@ -41,57 +53,42 @@ func MapToString(input map[string]string) []string {
 
 // Int64Value returns value from pointer.
 func Int64Value(v *int64) int64 {
-	if v == nil {
-		return 0
-	}
-	return *v
+	return PtrValue(v)
 }
 
 // Int32 returns pointer to int32.
 func Int32(value int32) *int32 {
-	return &value
+	return Ptr(value)
 }
 
 // Int32Value returns value from pointer.
 func Int32Value(v *int32) int32 {
-	if v == nil {
-		return 0
-	}
-	return *v
+	return PtrValue(v)
 }
 
 // UintValue returns value from pointer.
 func UintValue(v *uint) uint {
-	if v == nil {
-		return 0
-	}
-	return *v
+	return PtrValue(v)
 }
 
 // Float64Value returns value from pointer.
 func Float64Value(v *float64) float64 {
-	if v == nil {
-		return 0
-	}
-	return *v
+	return PtrValue(v)
 }
 
 // Float64 returns pointer to float64.
 func Float64(value float64) *float64 {
-	return &value
+	return Ptr(value)
 }
 
 // Bool returns a pointer to a bool.
 func Bool(v bool) *bool {
-	return &v
+	return Ptr(v)
 }
 
 // BoolValue returns the value of bool pointer or false.
 func BoolValue(v *bool) bool {
-	if v == nil {
-		return false
-	}
-	return *v
+	return PtrValue(v)
 }
 
 // StringToBool returns boolean value from string.
