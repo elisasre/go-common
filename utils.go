2208cef16319e4574c19eaa03c3d24a042f59cad
Jesse Haka <haka.jesse@gmail.com>
2023-02-06 13:12:54 +0200

    load and listen config file

--- utils.go
+++ utils.go
@@ -6,12 +6,24 @@ import (
 	"fmt"
 	"math/big"
 	"net/http"
+	"os"
 	"strings"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
 )
 
 const (
 	randomLength = 32
 )
+
+func init() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMs
+	log.Logger = zerolog.New(os.Stderr)
+	log.Logger = log.With().Logger()
+}
 
 var characterRunes = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
@@ -82,4 +94,34 @@ func RemoveDot(input string) string {
 		return input[:len(input)-1]
 	}
 	return input
+}
+
+// LoadAndListenConfig loads config file to struct and listen changes in it.
+func LoadAndListenConfig(path string, obj interface{}, onUpdate func()) error {
+	v := viper.New()
+	v.SetConfigFile(path)
+	if err := v.ReadInConfig(); err != nil {
+		return fmt.Errorf("unable to read config: %v", err)
+	}
+	if err := v.Unmarshal(&obj); err != nil {
+		return fmt.Errorf("unable to marshal config: %v", err)
+	}
+	log.Info().
+		Str("path", v.ConfigFileUsed()).
+		Msg("config loaded")
+	v.WatchConfig()
+	v.OnConfigChange(func(e fsnotify.Event) {
+		log.Info().
+			Str("path", e.Name).
+			Msg("config reloaded")
+		if err := v.Unmarshal(&obj); err != nil {
+			log.Fatal().
+				Str("path", e.Name).
+				Msgf("unable to marshal config: %v", err)
+		}
+		if onUpdate != nil {
+			onUpdate()
+		}
+	})
+	return nil
 }
