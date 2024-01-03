ae9797c41ab89334d00d349aa5b12356d9aedf08
Henri Koski <henri.t.koski@gmail.com>
2024-01-03 15:01:08 +0200

    Add httpserver module

---
+++ service/module/httpserver/server.go
@@ -0,0 +1,94 @@
+package httpserver
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+)
+
+type Opt func(s *Server) error
+
+type Server struct {
+	srv  *http.Server
+	ln   net.Listener
+	opts []Opt
+}
+
+// New creates Server module with given options.
+func New(opts ...Opt) *Server {
+	return &Server{
+		opts: opts,
+	}
+}
+
+// Init starts net.Listener after applying all options.
+// Options are applied in same order as they were provided.
+func (s *Server) Init() error {
+	s.srv = &http.Server{ReadHeaderTimeout: time.Second * 10}
+	for _, opt := range s.opts {
+		if err := opt(s); err != nil {
+			return fmt.Errorf("httpserver.Server Option error: %w", err)
+		}
+	}
+
+	ln, err := net.Listen("tcp", s.srv.Addr)
+	if err != nil {
+		return fmt.Errorf("failed to init listener: %w", err)
+	}
+
+	s.ln = ln
+	return nil
+}
+
+// URL returns server's URL and can be called after initialization.
+func (s *Server) URL() string {
+	if s.srv.TLSConfig == nil {
+		return "http://" + s.ln.Addr().String()
+	}
+	return "https://" + s.ln.Addr().String()
+}
+
+// Run starts serving http request and can be called after initialization.
+func (s *Server) Run() error {
+	err := s.srv.Serve(s.ln)
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+	return nil
+}
+
+// Stop calls shutdown for server.
+func (s *Server) Stop() error {
+	return s.srv.Shutdown(context.Background())
+}
+
+func (s *Server) Name() string {
+	return "httpserver.Server"
+}
+
+// WithServer sets http.Server for module.
+func WithServer(srv *http.Server) Opt {
+	return func(s *Server) error {
+		s.srv = srv
+		return nil
+	}
+}
+
+// WithAddr sets http.Server.Addr.
+func WithAddr(addr string) Opt {
+	return func(s *Server) error {
+		s.srv.Addr = addr
+		return nil
+	}
+}
+
+// WithHandler sets http.Server.Handler.
+func WithHandler(h http.Handler) Opt {
+	return func(s *Server) error {
+		s.srv.Handler = h
+		return nil
+	}
+}
---
+++ service/module/httpserver/server_test.go
@@ -0,0 +1,76 @@
+package httpserver_test
+
+import (
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/elisasre/go-common/service/module/httpserver"
+	"github.com/hashicorp/go-multierror"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestErrOpt(t *testing.T) {
+	optErr := errors.New("opt err")
+	srv := httpserver.New(func(s *httpserver.Server) error { return optErr })
+	err := srv.Init()
+	require.ErrorIs(t, err, optErr)
+}
+
+func TestListenErr(t *testing.T) {
+	srv := httpserver.New(httpserver.WithAddr(`sdf./43/s]\\][]"`))
+	err := srv.Init()
+	require.Error(t, err)
+}
+
+func TestHTTPS(t *testing.T) {
+	srv := httpserver.New(httpserver.WithServer(&http.Server{
+		Addr:              "127.0.0.1:0",
+		ReadHeaderTimeout: time.Second,
+		TLSConfig:         &tls.Config{}, //nolint:gosec
+	}))
+	err := srv.Init()
+	require.NoError(t, err)
+	require.Contains(t, srv.URL(), "https://127.0.0.1:")
+}
+
+func TestServer(t *testing.T) {
+	srv := httpserver.New(
+		httpserver.WithServer(&http.Server{ReadHeaderTimeout: time.Second}),
+		httpserver.WithAddr("127.0.0.1:0"),
+		httpserver.WithHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, "Hello")
+		})),
+	)
+
+	require.NotEmpty(t, srv.Name())
+	require.NoError(t, srv.Init())
+	wg := &multierror.Group{}
+	wg.Go(srv.Run)
+
+	assertGet(t, srv.URL(), "Hello")
+
+	assert.NoError(t, srv.Stop())
+	err := wg.Wait().ErrorOrNil()
+	require.NoError(t, err)
+}
+
+func assertGet(t testing.TB, url, body string) {
+	resp, err := http.Get(url) //nolint:gosec
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	assert.NoError(t, resp.Body.Close())
+	assert.Equal(t, body, string(data))
+}
