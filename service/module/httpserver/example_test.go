47bf75e084be3974cec66a731c37bdecd0e4af68
Henri Koski <henri.t.koski@gmail.com>
2024-01-09 11:58:45 +0200

    Add server example

---
+++ service/module/httpserver/example_test.go
@@ -0,0 +1,19 @@
+package httpserver_test
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/elisasre/go-common/service/module/httpserver"
+)
+
+func ExampleNew() {
+	httpserver.New(
+		httpserver.WithServer(&http.Server{ReadHeaderTimeout: time.Second}),
+		httpserver.WithAddr("127.0.0.1:0"),
+		httpserver.WithHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, "Hello")
+		})),
+	)
+}
