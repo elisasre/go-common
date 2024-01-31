873ef64e34e4abc0e4d8f6899babcc848177933f
Juho Majasaari <ext-juho.majasaari@elisa.fi>
2024-01-31 15:11:08 +0200

    add util function to get free local tcp port

--- utils.go
+++ utils.go
@@ -6,6 +6,7 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"math/big"
+	"net"
 	"net/http"
 	"os"
 	"runtime"
@@ -237,4 +238,19 @@ func addSpanTracer(handlers []gin.HandlerFunc) []gin.HandlerFunc {
 	handlers = handlers[:len(handlers)-1]
 	handlers = append(handlers, sentrySpanTracer(), lastElement)
 	return handlers
+}
+
+func GetFreeLocalhostTCPPort() (int, error) {
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		return 0, fmt.Errorf("failed to find free port to listen on: %w", err)
+	}
+	defer listener.Close()
+
+	tcpAddr, ok := listener.Addr().(*net.TCPAddr)
+	if !ok {
+		return 0, fmt.Errorf("failed to get port from listener")
+	}
+
+	return tcpAddr.Port, nil
 }
