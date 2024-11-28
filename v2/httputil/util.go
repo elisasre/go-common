557cb23de307bb7851f64889e578988a0811ed60
Rolf Ahrenberg <rolf.ahrenberg@elisa.fi>
2024-11-28 16:03:20 +0200

    Define integer format

--- v2/httputil/util.go
+++ v2/httputil/util.go
@@ -28,7 +28,7 @@ func (e ErrorResponse) Error() string {
 
 // ErrorResponse provides HTTP error response.
 type ErrorResponse struct {
-	Code      uint              `json:"code,omitempty" example:"400"`
+	Code      int64             `json:"code,omitempty" format:"int64" example:"400"`
 	Message   string            `json:"message" example:"Bad request"`
 	ErrorType string            `json:"error_type,omitempty" example:"invalid_scope"`
 	Params    map[string]string `json:"params,omitempty"`
